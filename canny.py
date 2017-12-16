b6ff2fbe2fafa34d0bbc779f4e2fe5759ac8fbf8
michaelyou <michaelseu2011@gmail.com>
2017-12-16 21:56:39 +0800

    Edge detection

---
+++ canny.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+image = cv2.imread('image/nami.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# image = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow("Blurred", image)
+
+canny = cv2.Canny(image, 30, 150)
+cv2.imshow("Canny", canny)
+cv2.waitKey(0)
